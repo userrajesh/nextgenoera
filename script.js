bdcf366c9a6f887ce511aa4730438aaf0f20f440
rajesh <rajesh.mandal123@gmail.com>
2026-03-10 14:58:24 +0530

    pushed  first time

---
+++ script.js
@@ -0,0 +1,32 @@
+// HERO SLIDER
+
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(){
+
+slides.forEach(slide=>{
+slide.classList.remove("active");
+});
+
+slides[index].classList.add("active");
+
+index++;
+
+if(index === slides.length){
+index = 0;
+}
+
+}
+
+setInterval(showSlide,4000);
+
+
+// MOBILE MENU
+
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click",()=>{
+nav.classList.toggle("active");
+});
